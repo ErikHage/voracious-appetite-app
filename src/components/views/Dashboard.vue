53fcb67af488b0a33ca1493f350e2f3ef86d6c42
Erik Hage <ehage4@gmail.com>
2025-08-12 22:11:12 -0400

    adopt uniform login/main feral app page

--- src/components/views/Dashboard.vue
+++ src/components/views/Dashboard.vue
@@ -1,22 +1,33 @@
 <template>
   <v-container>
-    <div class="text-center">
-      <h1>Voracious Appetite</h1>
-      <h3 class="ma-2">An Online Recipe Store</h3>
-      <v-img
-          :src="getAssetPath('ai-cardinal.jpg')"
-          alt="Logo"
-          contain
-          max-width="500"
-          class="mx-auto"
-      ></v-img>
-    </div>
+    <v-row class="justify-center">
+      <v-col cols="12" lg="6">
+        <div class="text-center mb-4">
+          <h1>Voracious Appetite</h1>
+          <h3 class="ma-2">An Online Recipe Store</h3>
+          <v-img
+              :src="getAssetPath('ai-cardinal.jpg')"
+              alt="Logo"
+              contain
+              max-width="500"
+              class="mx-auto"
+          ></v-img>
+        </div>
+
+        <v-card class="text-center">
+          <v-card-title>Let's Get Cooking!</v-card-title>
+          <v-card-text>
+            <v-btn @click="goToRecipesList">Recipes</v-btn>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 
-import { getAssetPath } from "@/utils/assets";
+import {getAssetPath} from "@/utils/assets";
 
 export default {
   name: 'Dashboard',
@@ -27,6 +38,10 @@ export default {
 
   methods: {
     getAssetPath,
+
+    goToRecipesList() {
+      this.$router.push("/recipes");
+    },
   },
 }
 </script>
